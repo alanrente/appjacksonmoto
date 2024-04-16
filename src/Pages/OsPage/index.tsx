84edbdd10c3362ab7b688e8b498396009cd0b02a
Alan <alan.rente@gmail.com>
2024-04-16 20:43:36 -0300

    acertando cards para ficarem com scroll

--- src/Pages/OsPage/index.tsx
+++ src/Pages/OsPage/index.tsx
@@ -1,18 +1,24 @@
 import "./style.css";
 import { useOsPage } from "./index.hook";
 import { CardOSComponent } from "../../components/CardOSComponent/index.component";
+import { Button } from "antd";
 
 export function OsPage() {
   const { ordensServico, excludeColumn, handleGetValor } = useOsPage();
 
   return (
     <div className="ospage-main">
-      {ordensServico.length > 0 &&
-        ordensServico.map((os, i) => (
-          <>
-            <CardOSComponent os={os} key={i.toString()} />
-          </>
-        ))}
+      <div className="ospage-novo">
+        <Button>Novo Serviço</Button>
+      </div>
+      <div className="ospage-cards">
+        {ordensServico.length > 0 &&
+          ordensServico.map((os, i) => (
+            <>
+              <CardOSComponent os={os} key={i.toString()} />
+            </>
+          ))}
+      </div>
     </div>
   );
 }
