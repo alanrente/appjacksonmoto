ca64d9cb61d5e3d8d04c91c1902ebd0703e7c162
Alan <alan.rente@gmail.com>
2024-05-01 03:56:41 -0300

    date picker

--- src/Pages/OsPage/hook.tsx
+++ src/Pages/OsPage/hook.tsx
@@ -11,25 +11,52 @@ import { getAllMecanicos } from "../../services/mecanicos.service";
 import { getAllClientes } from "../../services/clientes.service";
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { Alert } from "antd";
+import moment from "moment-timezone";
+import { CardOSComponent } from "../../components/CardOS";
 
 export function useOsPage() {
   const [visible, setvisible] = useState(false);
+  const [visibleSkeleton, setvisibleSkeleton] = useState(false);
+  const [dtInicioDtFim, setdtInicioDtFim] = useState({
+    dtInicio: moment().format("YYYY-MM-DD"),
+    dtFim: moment().format("YYYY-MM-DD"),
+  });
 
   const queryClient = useQueryClient();
 
   const { data: ordens } = useQuery({
     queryKey: ["ordens-servico"],
     queryFn: async () => {
-      const ordens: IOrdemServico[] = await getAllOs({});
+      const ordens: IOrdemServico[] = await getAllOs({
+        dtInicio: dtInicioDtFim.dtInicio,
+        dtFim: dtInicioDtFim.dtFim,
+      });
+      setvisibleSkeleton(false);
       return ordens;
     },
     gcTime: 0,
   });
 
+  const mutationGetFiltered = useMutation({
+    mutationFn: async () => {
+      return await getAllOs({
+        dtInicio: dtInicioDtFim.dtInicio,
+        dtFim: dtInicioDtFim.dtFim,
+      });
+    },
+    onError: (err) => {
+      Alert({ type: "error", message: err.message });
+      setvisibleSkeleton(false);
+    },
+    onSuccess() {
+      queryClient.invalidateQueries({ queryKey: ["ordens-servico"] });
+    },
+  });
+
   const mutationOrdens = useMutation({
-    mutationFn: (e: TClienteCreate & TMecanicoCreate) => {
+    mutationFn: async (e: TClienteCreate & TMecanicoCreate) => {
       const { mecanico, ...cliente } = e;
-      return createOS({
+      return await createOS({
         cliente,
         mecanico,
         servicos: [],
@@ -59,15 +86,29 @@ export function useOsPage() {
     });
   }
 
+  function retornaArrayElement() {
+    // if (!ordens) return [<Skeleton active key={"skeleton-os-page"} />];
+    if (ordens && ordens.length > 0)
+      return ordens.map((os, i) => (
+        <CardOSComponent os={os} key={i.toString()} />
+      ));
+
+    return [<>Não ha ordens cadastradas!</>];
+  }
+
   useEffect(() => {
     handleGetAll();
   }, []);
 
   return {
-    ordens,
     visible,
     autoComplete,
     mutationOrdens,
+    visibleSkeleton,
+    mutationGetFiltered,
     setvisible,
+    setdtInicioDtFim,
+    setvisibleSkeleton,
+    retornaArrayElement,
   };
 }
---
+++ src/interfaces/query.interface.ts
@@ -0,0 +1,7 @@
+export type QueryOrdemServico = {
+  mecanicoId?: number;
+  clienteId?: number;
+  includeTotais?: boolean;
+  dtInicio?: string;
+  dtFim?: string;
+};
